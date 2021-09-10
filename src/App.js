3700aea5c5c1307203afba1f0b764ea63e6a4249
Benjamin Bourgeois <benjishift@gmail.com>
2021-09-10 11:49:54 +0100

    :bug: fixed package

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import "./App.css";
+import Header from "./components/header/header.cmp";
+import AboutPage from "./pages/about/about.cmp";
+import ContactPage from "./pages/contact/contact.cmp";
+import HomePage from "./pages/homepage/homepage.cmp";
+import PortfolioPage from "./pages/portfolio/portfolio.cmp";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route
+            exact
+            path="/"
+            component={() => <HomePage handleClick={this.handleClick} />}
+          />
+          <Route exact path="/Portfolio/:title" component={PortfolioPage} />
+          <Route path="/about" component={AboutPage} />
+          <Route path="/contact" component={ContactPage} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
