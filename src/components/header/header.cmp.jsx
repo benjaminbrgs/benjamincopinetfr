78f614a7d1c096ac174e493836a996644061a820
Benjamin Bourgeois <benjishift@gmail.com>
2021-09-10 12:42:30 +0100

    :construction: add data

--- src/components/header/header.cmp.jsx
+++ src/components/header/header.cmp.jsx
@@ -1,37 +1,35 @@
-import React from 'react';
-import './header.scss';
-import Nav from '../../components/nav/nav.cmp';
-import BurgetButoon from '../burger/burger.cmp';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import Nav from "../../components/nav/nav.cmp";
+import BurgetButoon from "../burger/burger.cmp";
+import "./header.scss";
 
 class Header extends React.Component {
-	constructor() {
-		super();
-		this.state = {
-			nav: false
-		};
-	}
+  constructor() {
+    super();
+    this.state = {
+      nav: false,
+    };
+  }
 
-	buttonToggle = () => {
-		this.setState((prevstate) => {
+  buttonToggle = () => {
+    this.setState((prevstate) => {
       return { nav: !prevstate.nav };
-     
-		});
-	};
+    });
+  };
 
   render() {
-    
-		return (
-				<div className="header">
-					<Link className="logo" to="/">
-            <div className="logo-title">ILIYAN</div>
-            <div className="logo-line"></div>
-						<div className="logo-title">TSACHEV</div>
-					</Link>
-          <BurgetButoon toggle={this.buttonToggle} nav={this.state.nav} />
-					<Nav toggle={this.buttonToggle} nav={this.state.nav}/>
-				</div>
-		);
-	}
+    return (
+      <div className="header">
+        <Link className="logo" to="/">
+          <div className="logo-title">BENJAMIN</div>
+          <div className="logo-line"></div>
+          <div className="logo-title">COPINET</div>
+        </Link>
+        <BurgetButoon toggle={this.buttonToggle} nav={this.state.nav} />
+        <Nav toggle={this.buttonToggle} nav={this.state.nav} />
+      </div>
+    );
+  }
 }
 export default Header;
