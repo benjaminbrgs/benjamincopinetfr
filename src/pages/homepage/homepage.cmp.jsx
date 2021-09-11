426d650e16150475f01ae40da0bb57bf6c381460
Benjamin Bourgeois <benjishift@gmail.com>
2021-09-11 17:58:28 +0100

    :sparkles: homepage galeries with wp api rest

--- src/pages/homepage/homepage.cmp.jsx
+++ src/pages/homepage/homepage.cmp.jsx
@@ -8,54 +8,32 @@ class HomePage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      sections: [
-        {
-          id: 1,
-          title: "Landscape",
-          subtitle: "Paysage",
-          imageUrl:
-            "https://www.api.benjamincopinet.fr/wp-content/uploads/2021/09/landscape1.jpg",
-          revield: false,
-        },
-        {
-          id: 2,
-          title: "Wildlife",
-          subtitle: "Nature",
-          imageUrl:
-            "https://www.api.benjamincopinet.fr/wp-content/uploads/2020/02/benjamincopinet.fr-Wildlife-2.webp",
-          revield: false,
-        },
-        {
-          id: 3,
-          title: "Portrait",
-          subtitle: "Portrait",
-          imageUrl:
-            "https://www.api.benjamincopinet.fr/wp-content/uploads/2019/11/benjamincopinet.fr-portraits-002.jpg",
-          revield: false,
-        },
-        {
-          id: 4,
-          title: "Événementiel",
-          subtitle: "Événementiel",
-          imageUrl:
-            "https://www.api.benjamincopinet.fr/wp-content/uploads/2020/04/benjamincopinet.fr-evenementiel-1.jpg",
-          revield: false,
-        },
-        {
-          id: 5,
-          title: "Duo",
-          subtitle: "Duo",
-          imageUrl:
-            "https://www.api.benjamincopinet.fr/wp-content/uploads/2021/06/DSC05308-37.jpg",
-          revield: false,
-        },
-      ],
+      galeries: [],
     };
+  }
+
+  componentDidMount() {
+    return fetch("https://www.api.benjamincopinet.fr/wp-json/wp/v2/galerie")
+      .then((response) => response.json())
+      .then((data) => {
+        const galeries = data.map((galerie) => {
+          return {
+            id: galerie.id,
+            title: galerie.title.rendered,
+            subtitle: galerie.title.rendered,
+            imageUrl: galerie.acf.image_mise_en_avant,
+            revield: false,
+          };
+        });
+        this.setState({
+          galeries: galeries,
+        });
+      });
   }
 
   handleWaypoint = (index, action) => {
     this.setState((state) => {
-      const list = state.sections.map((item, i) => {
+      const list = state.galeries.map((item, i) => {
         if (index === i) {
           if (action === "enter") {
             return (item.revield = true);
@@ -77,12 +55,12 @@ class HomePage extends React.Component {
         <Slide>
           <Hero />
         </Slide>
-        {this.state.sections.map((item, index) => (
+        {this.state.galeries.map((item, index) => (
           <Slide key={item.id}>
             <ItemHome
               item={item}
               handleWaypoint={this.handleWaypoint}
-              reviel={this.state.sections}
+              reviel={this.state.galeries}
               index={index}
             />
           </Slide>
